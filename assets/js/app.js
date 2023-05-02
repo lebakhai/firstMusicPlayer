2333ec34b7c0b031578bf1e02ef7488d084b9d9e
lebakhai13 <lebakhai1312@gmail.com>
2023-05-02 21:44:18 +0700

    update UI and feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+var footerElement = $('.footer');
+var app;
+
 const url = 'http://84.46.246.159:1153/api/data';
 const options = {
 	method: 'GET',
@@ -8,4 +13,32 @@ const options = {
 
 fetch(url, options)
 .then((data) => data.json())
-.then(data => console.log(data.album))
+.then(data => {
+    app = data;
+    app.render = () => {
+        var htmls = app.album.map((song) => {
+            return `<div class="footer-item">
+            <div class="img-wrap">
+                <img src="${song.image}" class="song-img">
+            </div>
+            <div class="footer-info">
+                <h2 class="song-title">${song.title}</h2>
+                <div class="song-artist">${song.artist}</div>
+            </div>
+            <div class="footer-like">
+                <i class="fa-regular like-icon fa-heart"></i>
+            </div>
+        </div>`
+        })
+        var html = htmls.join(' ');
+        footerElement.innerHTML = html;
+    }
+    
+    app.start = () => {
+        app.render();
+    };
+    
+    app.start();
+})
+
+
