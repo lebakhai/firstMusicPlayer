778d813371da9226a9a682d3e84d16491ce4050c
lebakhai13 <lebakhai1312@gmail.com>
2023-05-03 16:02:34 +0700

    update UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-var footerElement = $('.footer');
+
+const footerElement = $('.footer');
+
 var app;
 
 // const url = 'http://84.46.246.159:1153/api/data';
@@ -35,8 +37,21 @@ fetch(url, options)
         footerElement.innerHTML = html;
     }
     
+    app.handleEvents = () => {
+        const infoElement = $('.info');
+        const infoElementWidth = infoElement.offsetHeight;
+
+        document.onscroll = () => {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            const newInfoElementWidth = infoElementWidth - scrollTop;
+            // console.log(infoElementWidth, scrollTop, newInfoElementWidth)
+            infoElement.style.height = newInfoElementWidth + "px";
+        };
+    }
+
     app.start = () => {
         app.render();
+        app.handleEvents();
     };
     
     app.start();
